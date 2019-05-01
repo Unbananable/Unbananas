4386b35f6077ef67f1d5ea644fbfc8c9bffabe02
Dimitri TRIGALOUX <dtrigalo@e3r7p21.42.fr>
2019-05-01 10:49:27 +0200

    update live

--- includes/corewar.h
+++ includes/corewar.h
@@ -6,7 +6,7 @@
 /*   By: anleclab <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/26 16:49:45 by anleclab          #+#    #+#             */
-/*   Updated: 2019/04/30 16:35:25 by dtrigalo         ###   ########.fr       */
+/*   Updated: 2019/04/30 18:03:18 by dtrigalo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,14 @@
 # include "libft.h"
 
 #include <stdio.h>
+
+#define CYCLE_LIVE 10
+
+typedef enum		e_bool
+{
+	true,
+	false
+}					t_bool;
 
 typedef struct		s_champ
 {
@@ -31,16 +39,28 @@ typedef struct		s_option //TODO marqueurs des differentes options
 	int				verbose; //TODO initialize apres parsing, prend une valeur, qui designe les donnees a afficher (cf resources/corewar)
 }					t_option;
 
+typedef struct		s_proc
+{
+	unsigned int	id; //TODO initialize a la creation d un proc (numero d identification du processus)
+	t_bool			carry; // TODO initialize a la creation d un proc
+	unsigned int	cooldown; // TODO initialize a la creation d un proc
+	unsigned int	idx; //TODO initialize a la creation d un proc (position dans l arene pour surbrillance)
+}					t_proc;
+
 typedef struct		s_cor
 {
+	t_proc			*procs; // TODO initialize after parsing, realloc after a fork/lfork
 	t_option		option;
 	int				nb_champs;
 	t_champ			**champs;
 	unsigned char	*arena;
 	unsigned int	curr_cycle; //TODO initialize at start after parsing
+	t_bool			last_alive; //TODO initialize
 }					t_cor;
 
 void			initialize(t_cor *cor);
+
+//int			instr_live(t_cor *cor, int idx);
 
 void			end(t_cor *cor);
 void			error(t_cor *cor, char *err_type);
