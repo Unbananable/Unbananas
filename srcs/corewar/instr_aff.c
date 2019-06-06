1735ce325cdba6aba0e8fe435c6e0e4fbca5e9a8
Dimitri TRIGALOUX <dtrigalo@student.42.fr>
2019-06-06 21:05:19 +0200

    update

--- srcs/corewar/instr_aff.c
+++ srcs/corewar/instr_aff.c
@@ -23,9 +23,15 @@ void		instr_aff(t_cor *cor, t_proc *proc)
     type = bits_peer_type(cor, proc, FIRST_PARAM);
     to_exec = (to_exec && type == REG_CODE);
     if (to_exec)
+    {
         arg1 = cor->arena[(proc->idx + proc->move + 1) % MEM_SIZE];
+        proc->carry = (!arg1);
+//      write(1, arg1, 1); 
+    }
     proc->move += byte_offset(type);
+/* *Partie a delete* */
     if (to_exec && arg1 < REG_NUMBER)
         ft_putchar((int)proc->regs[arg1] % 256);
+/* ** */
     proc->move += OPC_BYTE;
 }
