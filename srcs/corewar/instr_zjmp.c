c809b51f3e7f47252afe8adc44fb2be4a4988132
Anaelle LECLABART <an.leclabart@gmail.com>
2019-06-11 11:47:39 +0200

    remove print

--- srcs/corewar/instr_zjmp.c
+++ srcs/corewar/instr_zjmp.c
@@ -6,7 +6,7 @@
 /*   By: anleclab <anleclab@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/02 18:26:47 by dtrigalo          #+#    #+#             */
-/*   Updated: 2019/06/11 10:46:36 by anleclab         ###   ########.fr       */
+/*   Updated: 2019/06/11 11:30:38 by anleclab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,7 @@ void	instr_zjmp(t_cor *cor, t_proc *proc)
 
 	to_exec = (proc->carry);
 	if (to_exec)
-{printf("%d\n", get_short_arg_value(cor, proc->idx + 1) % IDX_MOD);
 		proc->move = get_short_arg_value(cor, proc->idx + 1) % IDX_MOD;
-}
 	else
 		proc->move = D2_BYTES + OPC_BYTE;
 }
