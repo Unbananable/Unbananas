ec545a4c842b4d51d045285d8244fec0d7df8542
Anaelle LECLABART <an.leclabart@gmail.com>
2019-06-18 13:25:37 +0200

    fluttershy works

--- srcs/corewar/instr_zjmp.c
+++ srcs/corewar/instr_zjmp.c
@@ -6,7 +6,7 @@
 /*   By: anleclab <anleclab@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/02 18:26:47 by dtrigalo          #+#    #+#             */
-/*   Updated: 2019/06/17 10:27:41 by anleclab         ###   ########.fr       */
+/*   Updated: 2019/06/18 13:24:49 by anleclab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 ** - wait: 20
 ** - argcode: no
 ** - args: DIR(2)
-** - addressing retriction: no
+** - addressing retriction: yes
 ** - carry: no
 ** - description: If the carry is 1, move the proc by the number of bytes
 **   indicated by the argument.
@@ -29,7 +29,7 @@ void	instr_zjmp(t_cor *cor, t_proc *proc)
 	t_bool	to_exec;
 
 	to_exec = proc->carry;
-	proc->move = get_short_arg_value(cor, proc->idx + 1) % IDX_MOD;
+	proc->move = get_short_arg_value(cor, proc->idx + 1);
 	if (cor->verbose & V_OPERATIONS)
 	{
 		ft_printf("P %4d | zjmp %d ", proc->n, proc->move);
@@ -38,6 +38,7 @@ void	instr_zjmp(t_cor *cor, t_proc *proc)
 		else
 			ft_printf("FAILED\n");
 	}
+	proc->move %= IDX_MOD;
 	if (!to_exec)
 		proc->move = D2_BYTES + OPC_BYTE;
 }
