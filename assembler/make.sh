c0aac5a9d7d544786b59ef2e7d5474af04282a94
Anas YAHYAOUI <anyahyao@e1r9p5.42.fr>
2019-07-02 23:25:34 +0200

    rien

---
+++ assembler/make.sh
@@ -0,0 +1,3 @@
+gcc check_instruction.c check_token.c error.c free.c ft_atoll.c ft_isspace.c ft_realloc.c get_next_line.c get_token.c init.c label.c op.c parsing.c recognize.c test.c token.c verify.c write_champion.c  libftprintf.a desas_instruction.c write_champion_code.c -I ../ft_printf/includes -I ../ft_printf/libft/inc/ desasssembler.c -o desasssembler
+
+gcc check_instruction.c check_token.c error.c free.c ft_atoll.c ft_isspace.c ft_realloc.c get_next_line.c get_token.c init.c label.c op.c parsing.c recognize.c test.c token.c verify.c write_champion.c  libftprintf.a desas_instruction.c write_champion_code.c -I ../ft_printf/includes -I ../ft_printf/libft/inc/ asm.c -o asm
